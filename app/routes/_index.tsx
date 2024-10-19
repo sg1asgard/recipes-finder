070b7e00f85f757436de1bb16791a639f784f15c
sg1asgard <sg1asgard@outlook.com>
2024-10-19 14:36:14 +0300

    updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { MetaFunction } from "@remix-run/node";
 import ButtonPrimary from "~/components/atoms/ButtonPrimary";
 import NavHeader from "~/components/NavHeader";
@@ -10,13 +11,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log("button clicked");
+  };
   return (
     <div className="container mx-auto">
       <NavHeader />
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <div className="mt-10">
-          <ButtonPrimary text="Click to count" />
+          <ButtonPrimary onClick={handleClick} text="Click to count" />
         </div>
       </main>
     </div>
