a8f9483c429d70b733c9fdc2e6f49ce16ed0015e
sg1asgard <sg1asgard@outlook.com>
2024-10-19 14:20:21 +0300

    updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,6 +14,9 @@ export default function Index() {
       <NavHeader />
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="mt-10">
+          <button className="bg-indigo-600 text-white py-2 px-4 rounded-full hover:bg-indigo-500">Count the clicks</button>
+        </div>
       </main>
     </div>
   );
