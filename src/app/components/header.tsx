cc2ed0584a2f5f4e6133dafb88353a0b86b03b9d
sg1asgard <sg1asgard@outlook.com>
2024-10-19 12:44:58 +0300

    updates

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,10 +7,12 @@ export default function Header() {
     <header>
       <div className="flex flex-row items-center py-4">
         <div className="flex-1">
-          <Image src={FoodSearchLogo} alt="food search logo" height={48} />
+          <Link href="/" className="hover:opacity-75 inline-block">
+            <Image src={FoodSearchLogo} alt="food search logo" height={48} />
+          </Link>
         </div>
         <div>
-            <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
         </div>
       </div>
     </header>
