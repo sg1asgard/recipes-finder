167d9c605b0cd902e51ef6600c12a72a7c87f7a3
sg1asgard <sg1asgard@outlook.com>
2024-10-19 12:37:39 +0300

    updates

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import FoodSearchLogo from "@/app/assets/images/food_search_logo.svg";
+
 export default function Header() {
-    return (
-        <header>
-            <div className="flex flex-row">
-                <div className="flex-1">logo</div>
-                <div>Nav items</div>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="flex flex-row items-center py-4">
+        <div className="flex-1">
+          <Image src={FoodSearchLogo} alt="food search logo" height={48} />
+        </div>
+        <div>Nav items</div>
+      </div>
+    </header>
+  );
 }
