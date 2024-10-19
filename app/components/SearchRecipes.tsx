c071bdcd2b52ec82016d8dd40fca8cbba85e83b7
sg1asgard <sg1asgard@outlook.com>
2024-10-19 16:02:10 +0300

    updates

--- app/components/SearchRecipes.tsx
+++ app/components/SearchRecipes.tsx
@@ -8,7 +8,6 @@ export default function SearchRecipes({
     <div className="flex flex-row">
       <input
         type="text"
-        value=""
         placeholder="Find a recipe..."
         className={`w-full px-6 py-4 rounded-full ${className}`}
       />
