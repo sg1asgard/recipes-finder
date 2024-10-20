fa3478b99d56e2f6ae60f73db2edb9639cb3532e
sg1asgard <sg1asgard@outlook.com>
2024-10-20 17:39:14 +0300

    updates

--- app/components/molecules/BreadcrumbNavigation.tsx
+++ app/components/molecules/BreadcrumbNavigation.tsx
@@ -1,16 +1,29 @@
+import { NavLink } from "@remix-run/react";
 
 interface BreadcrumbProps {
-    className?: string;
-    category?: string;
-    recipe?: string;
+  className?: string;
+  category?: string;
+  recipe?: string;
 }
 
-const Breadcrumb: React.FC<BreadcrumbProps> = ({ className, category, recipe }) => {
+const Breadcrumb: React.FC<BreadcrumbProps> = ({
+  className,
+  category,
+  recipe,
+}) => {
   return (
     <div>
-      <ul className={`flex flex-row ${className}`}>
-        <li>Home</li>
-        {category && <li>{category}</li>}
+      <ul className={`flex flex-row font-semibold ${className}`}>
+        <li>
+          <NavLink to="/" viewTransition className="hover:underline">
+            Home
+          </NavLink>
+        </li>
+        {category && (
+          <li className="flex flex-row space-x-2 ps-2">
+            <span className="">&gt;</span> <span>{category}</span>
+          </li>
+        )}
         {recipe && <li>{recipe}</li>}
       </ul>
     </div>
