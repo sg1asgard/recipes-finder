db0e871298aabe11d04830263cb2faebb248b574
sg1asgard <sg1asgard@outlook.com>
2024-10-20 12:37:26 +0300

    updates

--- app/routes/recipe.$id.tsx
+++ app/routes/recipe.$id.tsx
@@ -1,5 +1,12 @@
-export default function RecipeDetails() {
-    return (
-        <h1>Recipe details</h1>
-    )
-}
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  console.log(params);
+  return null;
+};
+
+const RecipeDetails = () => {
+  return <h1>Recipe details</h1>;
+};
+
+export default RecipeDetails;
