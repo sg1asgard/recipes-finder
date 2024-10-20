0fc1eaf4bc501745dedb7c257fa8d3181877bed7
sg1asgard <sg1asgard@outlook.com>
2024-10-20 15:10:16 +0300

    updates

--- app/components/SearchRecipes.tsx
+++ app/components/SearchRecipes.tsx
@@ -1,8 +1,7 @@
-import type { ActionFunction, ActionFunctionArgs } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import React, { useState } from "react";
-import ButtonPrimary from "./atoms/ButtonPrimary";
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
@@ -10,11 +9,7 @@ export const action: ActionFunction = async ({ request }) => {
 
   console.log("searched for:", recipeTitle);
 
-  // const response = await fetch('')
-  // const data = await response.json();
-
-  // return json({ mealsList: data.meals as FoodCategoryDetails[] });
-  return null;
+  return json({success: true, searchedFor: recipeTitle});
 };
 
 export default function SearchRecipes({
@@ -37,7 +32,6 @@ export default function SearchRecipes({
         className={`px-5 py-3 mx-auto bg-white border-2 shadow-sm focus:shadow-xl border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-amber-500 focus:ring-amber-500 block w-full rounded-full sm:text-sm focus:ring-1 disabled:shadow-none ${className}`}
       />
       <button type="submit">Clicked</button>
-      {/* <ButtonPrimary type="submit">Search</ButtonPrimary> */}
     </Form>
   );
 }
